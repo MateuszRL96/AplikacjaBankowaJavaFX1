13fcc1e763a89e1d2a7939e79f9cf92685f19628
MateuszRL96 <mati.kulec12@gmail.com>
2023-03-24 14:12:12 +0100

    Stworzenie pierwszego ekranu ładowania gdzie po urcuhomieniu wyświetlany zostaje panel logowania użytkownika, który w dalszej czesci bedzie zalezy od tego czy wybieramy admina czy uzytkownika, stworzenie małego logo z pomoca FontAwesome oraz nazwy. W formularzu mamy wybor pomiedzy Klientem a Adminem oraz Hasło, Emal uzytkownika, przycisk do zapisywania oraz Eroro który bedzie widoczny póxniej przy nieprawidłowym logowaniu się uzytkownika.

--- src/main/java/cam/jmc/aplikacjabankowajavafx/Controllers/LoginController.java
+++ src/main/java/cam/jmc/aplikacjabankowajavafx/Controllers/LoginController.java
@@ -1,4 +1,15 @@
 package cam.jmc.aplikacjabankowajavafx.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public TextField payee_address_fld;
+    public TextField password_fld;
+    public Button login_btn;
+    public Label payee_address_lbl;
+    public ChoiceBox acc_selector;
+    public Label error_lbl;
 }
