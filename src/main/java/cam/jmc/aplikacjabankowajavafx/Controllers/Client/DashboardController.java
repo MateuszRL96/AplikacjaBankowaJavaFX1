2faa60b4f00051593ea2325a35bc9c8cc4c82e0e
MateuszRL96 <mati.kulec12@gmail.com>
2023-03-27 16:31:46 +0200

    Stworzenie pierwszego ekranu ładowania gdzie po urcuhomieniu wyświetlany zostaje panel logowania użytkownika, który w dalszej czesci bedzie zalezy od tego czy wybieramy admina czy uzytkownika, stworzenie małego logo z pomoca FontAwesome oraz nazwy. W formularzu mamy wybor pomiedzy Klientem a Adminem oraz Hasło, Emal uzytkownika, przycisk do zapisywania oraz Eroro który bedzie widoczny póxniej przy nieprawidłowym logowaniu się uzytkownika.
    Stworzenie menu dostepnego dla klienta z kilkoma przyciskami takimi jak Transakcje, Profil czy Reportowanie usterek, Stworzenie ogólnegio wyglądu strony ze stylami bez ich funkcjonalnoci czytaj ActionListener itp.
    
    Stworzenie typowego podstawowego wygladu małego panelu dla Dashboard
    
    Stworzebnie wyglądu ogólnego pnalu klienta(DashBoard) z róznymi podstawowymi stylami CSS

--- src/main/java/cam/jmc/aplikacjabankowajavafx/Controllers/Client/DashboardController.java
+++ src/main/java/cam/jmc/aplikacjabankowajavafx/Controllers/Client/DashboardController.java
@@ -1,4 +1,29 @@
 package cam.jmc.aplikacjabankowajavafx.Controllers.Client;
 
-public class DashboardController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    public Label login_date;
+    public Text username;
+    public Label checking_balance;
+    public Label checking_account_number;
+    public Label savings_balance;
+    public Label saving_account_number;
+    public Label income_lbl;
+    public Label expense_lbl;
+    public ListView transaction_listview;
+    public TextField payee_field;
+    public TextField amount_field;
+    public TextArea message_field;
+    public Button send_money_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
