3cb8dae74c2d302c367ebfbcad0849611bed81df
MateuszRL96 <mati.kulec12@gmail.com>
2023-03-30 22:33:16 +0200

    Stworzenie pierwszego ekranu ładowania gdzie po urcuhomieniu wyświetlany zostaje panel logowania użytkownika, który w dalszej czesci bedzie zalezy od tego czy wybieramy admina czy uzytkownika, stworzenie małego logo z pomoca FontAwesome oraz nazwy. W formularzu mamy wybor pomiedzy Klientem a Adminem oraz Hasło, Emal uzytkownika, przycisk do zapisywania oraz Eroro który bedzie widoczny póxniej przy nieprawidłowym logowaniu się uzytkownika.
    Stworzenie menu dostepnego dla klienta z kilkoma przyciskami takimi jak Transakcje, Profil czy Reportowanie usterek, Stworzenie ogólnegio wyglądu strony ze stylami bez ich funkcjonalnoci czytaj ActionListener itp.
    
    Stworzenie typowego podstawowego wygladu małego panelu dla Dashboard
    
    Stworzebnie wyglądu ogólnego pnalu klienta(DashBoard) z róznymi podstawowymi stylami CSS
    
    Zabawa z ViewFactory.java
    Transactions
    Działa przełączanie pomiędzy Transactions a Dashbordem
    
    Stworzenie podstawowego wygladu strony dla Clienta, DashBoard, Transactions oraz Accounts.
    
    Stworzenie menu do zarzadzania9tworzenia klientów prxzez Admina.
    
    Stworzenie menu wyboru czy logujemy sie jako admin czy jako klient oraz odpowiednie wyswietlenie strony zaleznie od naszego wyboru
    
    Nie chce mis ei commitować, troche działa, troche nie. Różnie w zyciu dzieje się !
    
    Tabelki klientów, usuwanie, itp..

---
+++ src/main/java/cam/jmc/aplikacjabankowajavafx/Controllers/Admin/ClientCellController.java
@@ -0,0 +1,24 @@
+package cam.jmc.aplikacjabankowajavafx.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientCellController implements Initializable {
+    
+    public Label FirstName_label;
+    public Label LastName_label;
+    public Label pAddress_label;
+    public Label checking_account_label;
+    public Label saving_account_label;
+    public Label date_label;
+    public Button delete_button;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
